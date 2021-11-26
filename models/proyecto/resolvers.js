9b199a2b1a908f288e620ffd11887295b269999d
Carlos Hernandez <carlosheragu2003@gmail.com>
2021-11-26 13:58:26 -0500

    Se agrego tipos y resolvers para conectar con el front

--- models/proyecto/resolvers.js
+++ models/proyecto/resolvers.js
@@ -5,6 +5,15 @@ const resolversProyecto = {
     Proyectos: async (parent, args) => {
       const proyectos = await ProjectModel.find().populate('avances').populate('inscripciones');
       return proyectos;
+      // const proyectos = await ProjectModel.find()
+      //   .populate({
+      //     path: 'avances',
+      //     populate: {
+      //       path: 'creadoPor',
+      //     },
+      //   })
+      //   .populate('lider');
+      //   return proyectos
     },
     Proyecto: async (parent, args) => {
       const proyecto = await ProjectModel.findOne({ _id: args._id }).populate('avances').populate('inscripciones');
